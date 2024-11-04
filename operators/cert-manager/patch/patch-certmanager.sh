41c30fcb2fa85c4ead4f4cc5f200c1ac3be017d1
Gurney Buchanan <gurney.buchanan@gmail.com>
2024-11-04 09:56:23 -0500

    Update cert-manager patch

--- operators/cert-manager/patch/patch-certmanager.sh
+++ operators/cert-manager/patch/patch-certmanager.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-oc patch clusterserviceversion cert-manager.v1.10.2 \
+CSV_NAME=$(oc get csv -n openshift-operators | grep cert-manager | awk '{print $1}')
+CSV_VERSION=$(oc get csv -n openshift-operators $CSV_NAME -o jsonpath='{.spec.version}')
+echo "$CSV_VERSION"
+oc patch clusterserviceversion cert-manager.v$CSV_VERSION \
     --type=json \
     -n openshift-operators \
-    --patch='[{"op":"add","path":"/spec/install/spec/deployments/0/spec/template/spec/containers/0/args/-","value":"--dns01-recursive-nameservers-only"},{"op":"add","path":"/spec/install/spec/deployments/0/spec/template/spec/containers/0/args/-","value":"--dns01-recursive-nameservers=8.8.8.8:53,1.1.1.1:53"}]'
+    --patch='[{"op":"add","path":"/spec/install/spec/deployments/0/spec/template/spec/containers/0/args/-","value":"--dns01-recursive-nameservers-only"},{"op":"add","path":"/spec/install/spec/deployments/0/spec/template/spec/containers/0/args/-","value":"--dns01-recursive-nameservers=8.8.8.8:53,1.2.3.4:53"}]'
